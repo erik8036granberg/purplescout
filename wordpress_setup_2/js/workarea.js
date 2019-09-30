b2ad57f311898061e0a5a4a6b3f484f962ab630c
Erik Granberg <36399140+erik8036granberg@users.noreply.github.com>
2019-09-30 22:25:24 +0200

    work area js wp-connect

--- wordpress_setup_2/js/workarea.js
+++ wordpress_setup_2/js/workarea.js
@@ -52,4 +52,58 @@ function InsertPageContent(pageContent) {
 
   dest.querySelector("[data-showreel_header]").textContent =
     pageContent.acf.area_header;
+
+  // - - - - - - - - - - - Symbol & header - - - - - - - - - - -
+
+  dest
+    .querySelector("[data-area_symbol]")
+    .setAttribute("src", pageContent.acf.area_symbol);
+  dest.querySelector("[data-area_header]").textContent =
+    pageContent.acf.area_header;
+
+  // - - - - - - - - - - - main text - - - - - - - - - - -
+
+  dest.querySelector("[data-area_main_text]").innerHTML =
+    pageContent.acf.area_main_text;
+
+  // - - - - - - - - - - - get related cases - - - - - - - - - - -
+
+  // get cases
+  fetch("/wordpress/wp-json/wp/v2/case?per_page=100")
+    .then(response => response.json())
+    .then(myJson => {
+      let caseArray = myJson;
+      caseArray.forEach(caseItem => {
+        console.log(caseItem.acf.work_areas_symbols);
+        console.log(caseItem.acf.work_areas_symbols.includes(pageContent.id));
+        if (caseItem.acf.work_areas_symbols.includes(pageContent.id)) {
+          showRelatedCases(caseItem);
+        }
+      });
+    });
+}
+
+// - - - - - - - - - - - - - display related cases - - - - - - - - - - - - -
+
+function showRelatedCases(caseItem) {
+  console.log("showRelatedCases");
+  const template = document.querySelector("[data-related_template]").content;
+  const clone = template.cloneNode(true);
+
+  clone.querySelector("[data-related_case]").textContent = caseItem.acf.company;
+
+  // clone.querySelector("[data-id]").setAttribute("id", caseItem.slug);
+  // clone
+  //   .querySelector("[data-video_still_image]")
+  //   .setAttribute("src", caseItem.acf.video_still_image.sizes.medium_large);
+  // clone
+  //   .querySelector("[data-video_still_image]")
+  //   .setAttribute("alt", caseItem.acf.company);
+  // clone.querySelector("[data-company]").textContent = caseItem.acf.company;
+  // clone.querySelector("[data-solution]").textContent = caseItem.acf.solution;
+  // clone.querySelector("[data-id]").addEventListener("click", () => {
+  //   window.location.href = "case.html?id=" + caseItem.slug;
+  //   window.sessionStorage.setItem("caseLink", caseItem.slug);
+  // });
+  document.querySelector("[data-related_container]").appendChild(clone);
 }
