df506503aa736cb8c38159d6558a9aacd499ce9a
Erik Granberg <36399140+erik8036granberg@users.noreply.github.com>
2019-10-18 22:04:43 +0200

    case page quote css fixes

--- js/index.js
+++ js/index.js
@@ -330,24 +330,25 @@ async function getTestimonialContent() {
   <polygon class="cls-1 outline" points="58.5 578.5 118.09 507.5 0.5 507.5 0.5 0.5 447.5 0.5 447.5 507.5 166.18 507.5 58.5 578.5"/>
 </svg>`;
   document.querySelector("#intro #quote_outline").innerHTML = bubble_ouline;
-  document.querySelector("#intro .swop_frame").onmouseenter = function() {
-    document.querySelector("#intro svg .outline").classList.add("hover");
-  };
-  document.querySelector("#intro .swop_frame").onmouseleave = function() {
-    document.querySelector("#intro svg .outline").classList.remove("hover");
-  };
   displayTestimonial(filteredTestimonialArray[sw_i]);
 }
 
 function displayTestimonial(testimonial) {
   caseArray.forEach(caseItem => {
     if (testimonial.acf.related_case.includes(caseItem.id)) {
-      console.log("case & quote match");
       document.querySelector("#intro .bubble").addEventListener("click", () => {
         window.location.href = "case.html?id=" + caseItem.slug;
         window.sessionStorage.setItem("caseLink", caseItem.slug);
         window.sessionStorage.setItem("indexScroll", indexScrollTop);
       });
+      document.querySelector("#intro .swop_frame").onmouseenter = function() {
+        document.querySelector("#intro svg .outline").classList.add("hover");
+        document.querySelector("#intro .bubble").style.cursor = "pointer";
+      };
+      document.querySelector("#intro .swop_frame").onmouseleave = function() {
+        document.querySelector("#intro svg .outline").classList.remove("hover");
+        document.querySelector("#intro .bubble").style.cursor = "none";
+      };
     }
   });
   document.querySelector("#intro .bubble").classList.remove("fade_out");
